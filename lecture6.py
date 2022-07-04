90ff442b44fb6816596f28cbd6df2655b5b116ff
GeorgKovalev <106740753+GeorgKovalev@users.noreply.github.com>
2022-07-04 20:50:24 +0300

    Add files via upload

--- lecture6.py
+++ lecture6.py
@@ -5,64 +5,30 @@ import random
 from datetime import datetime
 import time
 from time import sleep
+from stp_reader import stp_reader
 
-def read_stp(file_name : str):
-    file = open(file_name, 'r')
-
-    file_text = file.readlines()
-
-    start_index = -1
-    for i in range(len(file_text)):
-        if file_text[i].startswith('SECTION Graph'):
-            start_index = i
-            break
-    if start_index == -1:
-        print("ERROR!")
-        return {}
-
-    graph = {'E': [], 'A':[], 'N': 0}
-    cnt_info_lines = 0
-    
-    start_index += 1
-    while file_text[start_index].startswith(('Nodes','Arcs', 'Edges')):
-        words = file_text[start_index].split()
-        if words[0] == 'Nodes':
-            graph['N'] = int(words[1])
-        else:
-            cnt_info_lines += int(words[1])
-        start_index += 1
-
-    for i in range(cnt_info_lines):
-        line = file_text[start_index + i].split()
-        graph[line[0]].append((int(line[1]), int(line[2]), float(line[3])))
-    file.close()
-    return graph
 
 dfs_nodes_order = []
 
 def draw_graph(elist, cnt_v):
     G = nx.DiGraph() # создание графа
-    for i in range(1, cnt_v):
-        G.add_node(i)
 
     nodes = [i + 1 for i in range(G.number_of_nodes())]
     G.add_weighted_edges_from(elist) # добавить список взвешенных рёбер
 
-    labels = nx.get_edge_attributes(G, 'weight') #
+    labels = nx.get_edge_attributes(G, 'weight')
     # упаковка графа на плоскость. существует несколько возможных конфигураций (layout)
     random.seed(time.time())
     pos = nx.circular_layout(G)
 
     fig, ax = plt.subplots(figsize=(len(elist), 8))
     # список в порядке dfs
-    
+
     def update(idx):
-        ax.clear()
+        colors_set = ["red" for i in range(cnt_v)]
+        colors_set[dfs_nodes_order[idx]] = "green"
         # рендер вершин
-        colors_set = []
-        for i in range(cnt_v):
-            colors_set.append( (1, 0, 0) )
-        colors_set[dfs_nodes_order[idx]] = (0, 1, 0)
+        print(colors_set)
         nx.draw_networkx_nodes(G, pos, ax=ax, node_color=colors_set)
     
         # рендер рёбер
@@ -83,28 +49,37 @@ def draw_graph(elist, cnt_v):
     plt.show()
 
 def main():
-    mp = read_stp("simple.stp")
-    using_character = 'A'
-    other_character = 'E'
-    
-    elist = mp[using_character] + mp[other_character]
-    adj_list = [[] for i in range(mp['N'])]
-    for edges in mp[using_character]:
-        adj_list[edges[0] - 1].append(edges[1] - 1)
+    elist, alist, node_count = stp_reader("simple.stp")
 
-    visited = [False] * mp['N']
+    alist += elist # don't feel difference between nodes and arcs
+    # optionally: split edge to 2 arcs
+    # for edge in elist:
+    #     alist += edge
+    #     alist += (edge[1], edge[0], edge[2])
+
+    adj_list = [[] for i in range(node_count)]
+    for arcs in alist:
+        adj_list[int(arcs[0]) - 1].append(int(arcs[1]) - 1)
+
     start_v = 1 #int(input("Input start node!"))
-    dfs(adj_list, visited, start_v - 1)
-    draw_graph(elist, mp['N'])
+    dfs(adj_list, start_v - 1)
+    print(alist, node_count)
+    draw_graph(alist, node_count)
 
-def dfs(adj_list, visited, cur_v):
-        if visited[cur_v] == True:
-            return
+def dfs(adj_list, start_v):
+        stack = []
+        v_colors = [0 for i in range(len(adj_list))] # in the beginning all nodes are white
+        stack.append(start_v)
 
-        visited[cur_v] = True
-        dfs_nodes_order.append(cur_v)
-        for neighbour in adj_list[cur_v]:
-            dfs(adj_list, visited, neighbour)
+        while stack != []:
+            cur_v = stack.pop()
+            dfs_nodes_order.append(cur_v)
+            for neighbour in adj_list[cur_v]:
+                if v_colors[neighbour] == 0:
+                    v_colors[cur_v] = 1 # grey
+                    stack.append(neighbour)
+            v_colors[cur_v] = 2 # black
+
 
 if __name__ == "__main__":
     main()
